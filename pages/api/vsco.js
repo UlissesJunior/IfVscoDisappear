54d5314420314adc85f94d17f07ebe38d5fba67b
UlissesJunior <ulissesjunior2005@gmail.com>
2023-02-15 23:57:06 -0300

    First Commit

---
+++ pages/api/vsco.js
@@ -0,0 +1,105 @@
+import puppeteer from "puppeteer";
+import fetch from "node-fetch";
+import JSZip from "jszip";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+
+export default async function handler(req, res) {
+  if (req.method === "POST" && req.url === "/api/vsco") {
+    const { username } = req.body;
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto(`https://vsco.co/${username}/gallery`);
+
+    if ((await page.title()) !== "Page Not Found | VSCO") {
+      let loadMoreButton = await page.$('[class="css-oww03x e1xqpt600"]');
+      while (loadMoreButton !== null) {
+        await loadMoreButton.click();
+        await page
+          .waitForSelector('[class="css-oww03x e1xqpt600"]', { timeout: 3000 })
+          .catch(() => console.log("Load more button disappeared"));
+        loadMoreButton = await page.$('[class="css-oww03x e1xqpt600"]');
+      }
+
+      const scrollStep = 1000;
+      const scrollDelay = 10;
+      let scrollCount = 0;
+
+      while (scrollCount < 1200) {
+        await page.evaluate((scrollStep) => {
+          window.scrollBy(0, scrollStep);
+        }, scrollStep);
+
+        scrollCount++;
+        await page.waitForTimeout(scrollDelay);
+      }
+
+      const imgList = await page.evaluate(() => {
+        const vscoList = document.querySelectorAll("section div figure a");
+        const imgArray = [...vscoList];
+        const imgList = imgArray.map((a) => ({
+          href: a.href,
+        }));
+        return imgList;
+      });
+
+      const imageSrcList = [];
+
+      for (let i = 0; i < imgList.length; i++) {
+        const imgPage = await browser.newPage();
+        await imgPage.goto(imgList[i].href);
+        await imgPage.waitForSelector(
+          "body > div > div > main > div > div > div > div > div > img"
+        );
+        const imgSrc = await imgPage.evaluate(() => {
+          const img = document.querySelector(
+            "body > div > div > main > div > div > div > div > div > img"
+          );
+          return img ? img.src : null;
+        });
+        imageSrcList.push(imgSrc);
+        await imgPage.close();
+      }
+
+      const uniqueImageSrcList = Array.from(new Set(imageSrcList));
+
+      const startTime = new Date();
+      const images = [];
+      for (let i = 0; i < uniqueImageSrcList.length; i++) {
+        const response = await fetch(uniqueImageSrcList[i]);
+        const buffer = await response.buffer();
+        images.push({ name: `${i}.jpg`, buffer });
+      }
+
+      const zip = new JSZip();
+      for (const image of images) {
+        zip.file(image.name, image.buffer);
+      }
+      const zipBuffer = await zip.generateAsync({ type: "nodebuffer" });
+
+      const endTime = new Date();
+      const timeDiff = (endTime - startTime) / 1000;
+
+      const filename = `${uuidv4()}.zip`;
+
+      fs.writeFileSync(filename, zipBuffer);
+
+      await browser.close();
+      res.setHeader("Content-Type", "application/zip");
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename="${filename}"`
+      );
+      res.setHeader("Content-Length", zipBuffer.length);
+      res.send(zipBuffer);
+
+      res.status(303).setHeader("Location", "/").end();
+    } else {
+      await browser.close();
+      res.status(303).setHeader("Location", "/").end();
+    }
+  } else {
+    res.status(404);
+  }
+}
---
+++ src/styles/styles.js
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  img {
+    position: absolute;
+    top: 10vh;
+  }
+`;
+
+export const Input = styled.input`
+  width: 500px;
+  height: 60px;
+  border: 4px solid #000;
+  border-radius: 6px;
+  font-size: 1.2em;
+  padding: 6px;
+  position: relative;
+  @media (max-width: 1100px) {
+   width: 400px;
+  }
+  @media (max-width: 420px) {
+    width: 240px;
+   }
+`;
+
+export const Button = styled.button`
+  width: 5vw;
+  height: 60px;
+  background-color: #000;
+  border-radius: 6px;
+  position: absolute;
+  right: 0;
+  img {
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin-top: auto;
+    margin-bottom: auto;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  @media (max-width: 1000px) {
+    width: 60px;
+  }
+  @media (max-width: 420px) {
+    width: 40px;
+   }
+`;
+
+export const DivButton = styled.div`
+  width: 500px;
+  height: 60px;
+  position: relative;
+  @media (max-width: 1100px) {
+    width: 400px;
+   }
+   @media (max-width: 420px) {
+    width: 240px;
+   }
+`;
